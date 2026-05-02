48047801243a944eb0934e6d1df8376bafd67356
Simone Gauli <simonegauli@gmail.com>
2026-05-02 16:58:45 +0200

    chore: remove redunant script

--- tools/release.ts
+++ tools/release.ts
@@ -1,6 +1,6 @@
 import { execa } from 'execa';
 
-await execa('yarn', ['typecheck'], { stdio: 'inherit' });
+await execa('yarn', ['tsc'], { stdio: 'inherit' });
 await execa('yarn', ['lint'], { stdio: 'inherit' });
 await execa('yarn', ['version', 'minor'], { stdio: 'inherit' });
 await execa('yarn', ['prepare'], { stdio: 'inherit' });
