644de8820567f9167a6d4bd109fbdb05dd70ab44
Simone Gauli <simonegauli@gmail.com>
2026-06-07 10:51:41 +0200

    fix(release): add prepack

--- tools/release.ts
+++ tools/release.ts
@@ -1,7 +1,6 @@
 import { execa } from 'execa';
 
 // yarn dlx @goatjs/dbz login
-await execa('yarn', ['prepare'], { stdio: 'inherit' });
 await execa('yarn', ['dlx', '@goatjs/dbz', 'publish', '--release'], {
   stdio: 'inherit',
 });
